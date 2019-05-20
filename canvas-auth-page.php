b0cc428e5fa02f4ce5847888d954bb4ea5351a1f
Jeff Everhart <Jeffeverhart383@gmail.com>
2019-05-20 13:31:40 -0400

    rebuild after realizing I'm an idiot

--- canvas-auth-page.php
+++ canvas-auth-page.php
@@ -1,11 +1,12 @@
 <h2>Canvas Auth Page</h2>
 <h3>Here is some stuff</h3>
+<p>Here is a paragraph</p>
 
 
 <?php
+  include dirname(__FILE__) . '/credentials.php';
+  $canvas_url = sprintf($url, $canvas_client_id, $redirect_uri);
 
-// include dirname(__FILE__) . '/credentials.php';
-  // $canvas_url = sprintf("https://virginiacommonwealth.test.instructure.com/login/oauth2/auth?client_id=%s&response_type=code&redirect_uri=https://rampages.us", $canvas_client_id);
 ?>
 
-<a>OAuth with Canvas</a>
+<a href="<?php echo $canvas_url; ?>">OAuth with Canvas</a>
