d376665f294c87de89d03a31c02189d4a2ed6df9
Jeff Everhart <Jeffeverhart383@gmail.com>
2019-05-20 10:36:30 -0400

    Update canvas-auth-page.php

--- canvas-auth-page.php
+++ canvas-auth-page.php
@@ -1,11 +1,11 @@
 <h2>Canvas Auth Page</h2>
-
+<h3>Here is some stuff</h3>
 
 
 <?php
 
-include dirname(__FILE__) . '/credentials.php';
-  $canvas_url = sprintf("https://virginiacommonwealth.test.instructure.com/login/oauth2/auth?client_id=%s&response_type=code&redirect_uri=https://rampages.us", $canvas_client_id);
+// include dirname(__FILE__) . '/credentials.php';
+  // $canvas_url = sprintf("https://virginiacommonwealth.test.instructure.com/login/oauth2/auth?client_id=%s&response_type=code&redirect_uri=https://rampages.us", $canvas_client_id);
 ?>
 
-<a href=<?php echo $canvas_url; ?>>OAuth with Canvas</a>
+<a>OAuth with Canvas</a>
