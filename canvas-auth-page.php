d2a334dcd2e9ff56d72936ec95d2f18c4d79fa8e
Jeff Everhart <Jeffeverhart383@gmail.com>
2019-05-20 10:28:46 -0400

    update include statement

--- canvas-auth-page.php
+++ canvas-auth-page.php
@@ -4,7 +4,7 @@
 
 <?php
 
-  require_once './credentials.php';
+include dirname(__FILE__) . '/credentials.php';
   $canvas_url = sprintf("https://virginiacommonwealth.test.instructure.com/login/oauth2/auth?client_id=%s&response_type=code&redirect_uri=https://rampages.us", $canvas_client_id);
 ?>
 
